7b980d2d999f822d3b8fe7fcc17e8736d2c15a36
Lofibree <nastenkapersik17@gmail.com>
2022-09-16 16:56:25 +0300

    second commit

--- ScrollBg.js
+++ ScrollBg.js
@@ -1,75 +1,19 @@
 
-function ScrollBg() {
-    this.bgImgEl = null;
+class ScrollBg {
+    constructor() {
+        this.bgImgEl = null;
 
-    this.start = function(elId) {
-        var that = this;
-        var elId = document.querySelector('#' + elId);
-        this.bgImgEl = elId.querySelector('#bgImg');
+        this.start = function (elId) {
+            var that = this;
+            var elId = document.querySelector('#' + elId);
+            this.bgImgEl = elId.querySelector('#bgImg');
 
-        window.addEventListener('scroll', () => {
-            that.bgImgEl.style.opacity = 1 - window.pageYOffset / 800;
-            console.log(window.pageYOffset, 1 - window.pageYOffset / 800);
-            that.bgImgEl.style.backgroundSize = 230 - (window.pageYOffset / window.innerHeight * 30) + 'vh';
-            console.log(that.bgImgEl.style.backgroundSize);
-        })
+            window.addEventListener('scroll', () => {
+                that.bgImgEl.style.opacity = 1 - window.pageYOffset / 800;
+                console.log(window.pageYOffset, 1 - window.pageYOffset / 800);
+                that.bgImgEl.style.backgroundSize = 230 - (window.pageYOffset / window.innerHeight * 30) + 'vh';
+                console.log(that.bgImgEl.style.backgroundSize);
+            });
+        };
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const bgImgEl = document.getElementById('bgImg');
-// window.addEventListener('scroll', () => {
-//     updateImg()
-// });
-
-// function updateImg() {
-//     bgImgEl.style.opacity = 1 - window.pageYOffset / 800;
-//     console.log(window.pageYOffset, 1 - window.pageYOffset / 800);
-//     bgImgEl.style.backgroundSize = 230 - (window.pageYOffset / window.innerHeight * 30) + 'vh';
-//     console.log(bgImgEl.style.backgroundSize);
-// }
-
-
-
-
-
-
-
-
-
-
-
-// const bgImgEl = document.getElementById('bgImg');
-// window.addEventListener('scroll', () => {
-//     updateImg()
-// });
-
-// function updateImg() {
-//     bgImgEl.style.opacity = 1 - window.pageYOffset / 800;
-//     console.log(window.pageYOffset, 1 - window.pageYOffset / 800);
-//     bgImgEl.style.backgroundSize = 230 - (window.pageYOffset / window.innerHeight * 30) + 'vh';
-//     console.log(bgImgEl.style.backgroundSize);
-// }
